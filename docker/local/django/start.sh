34b41665272683caf47cfc822648c9b5f810f83f
abdelslam1997 <51970604+abdelslam1997@users.noreply.github.com>
2024-12-12 09:20:58 +0400

    Integrate daphne with runserver command for hot reloads

--- docker/local/django/start.sh
+++ docker/local/django/start.sh
@@ -7,5 +7,8 @@ set -o nounset
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
-# Fix: Update daphne command to use correct ASGI application path
-daphne -b 0.0.0.0 -p 8000 light_messages.asgi:application
+### Run daphne server
+## daphne -b 0.0.0.0 -p 8000 light_messages.asgi:application
+
+### Run with runserver
+python manage.py runserver
