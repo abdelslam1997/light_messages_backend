d4192b5e70ef20190995a9e38581b7826ff472a4
abdelslam1997 <51970604+abdelslam1997@users.noreply.github.com>
2025-01-11 22:14:34 +0400

    Update Production Settings S3 : build-docker-image

--- light_messages/settings/production.py
+++ light_messages/settings/production.py
@@ -10,19 +10,17 @@ STORAGES = {
     "default": {
         "BACKEND": "storages.backends.s3.S3Storage",
         "OPTIONS": {
-            "AWS_S3_ACCESS_KEY_ID": env("AWS_S3_ACCESS_KEY_ID"),
-            "AWS_S3_SECRET_ACCESS_KEY": env("AWS_S3_SECRET_ACCESS_KEY"),
-            "AWS_STORAGE_BUCKET_NAME": env("AWS_STORAGE_BUCKET_NAME"),
-            "AWS_S3_CUSTOM_DOMAIN": f"{env('AWS_STORAGE_BUCKET_NAME')}.s3.amazonaws.com",
+            "access_key": env.str("AWS_S3_ACCESS_KEY_ID"),
+            "secret_key": env.str("AWS_S3_SECRET_ACCESS_KEY"),
+            "bucket_name": env.str("AWS_STORAGE_BUCKET_NAME"),
         },
     },
     "staticfiles": {
         "BACKEND": "storages.backends.s3.S3Storage",
         "OPTIONS": {
-            "AWS_S3_ACCESS_KEY_ID": env("AWS_S3_ACCESS_KEY_ID"),
-            "AWS_S3_SECRET_ACCESS_KEY": env("AWS_S3_SECRET_ACCESS_KEY"),
-            "AWS_STORAGE_BUCKET_NAME": env("AWS_STORAGE_BUCKET_NAME"),
-            "AWS_S3_CUSTOM_DOMAIN": f"{env('AWS_STORAGE_BUCKET_NAME')}.s3.amazonaws.com",
+            "access_key": env.str("AWS_S3_ACCESS_KEY_ID"),
+            "secret_key": env.str("AWS_S3_SECRET_ACCESS_KEY"),
+            "bucket_name": env.str("AWS_STORAGE_BUCKET_NAME"),
             "location": "static",
         },
     },
