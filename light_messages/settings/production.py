027cbfec40338d0890026bd692389d00c1a08c1e
abdelslam1997 <51970604+abdelslam1997@users.noreply.github.com>
2025-01-11 22:53:47 +0400

    Update Production Settings S3 : build-docker-image

--- light_messages/settings/production.py
+++ light_messages/settings/production.py
@@ -9,11 +9,20 @@ INSTALLED_APPS += ["storages"]
 STORAGES = {
     "default": {
         "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+        "OPTIONS": {
+            "location": "media",
+        },
     },
     "staticfiles": {
         "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
         "OPTIONS": {
-            "location": "static",  # Only the prefix
+            "location": "static",
         },
     },
 }
+
+AWS_ACCESS_KEY_ID = env.str("AWS_S3_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = env.str("AWS_S3_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = env.str("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME = env.str("AWS_S3_REGION_NAME")
+AWS_S3_SIGNATURE_VERSION = env.str("AWS_S3_SIGNATURE_VERSION")
