a4cf44435a6a8ea387dc421dc67ed61655fc0740
abdelslam1997 <51970604+abdelslam1997@users.noreply.github.com>
2025-01-12 09:01:39 +0400

    Update Production Settings S3 : build-docker-image

--- light_messages/settings/production.py
+++ light_messages/settings/production.py
@@ -1,28 +1,46 @@
 from .base import *  # noqa
 from .base import env
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 INSTALLED_APPS += ["storages"]
 
+# AWS S3 Base Configuration
+AWS_ACCESS_KEY_ID = env.str("AWS_S3_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = env.str("AWS_S3_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = env.str("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME = env.str("AWS_S3_REGION_NAME")
+AWS_S3_SIGNATURE_VERSION = env.str("AWS_S3_SIGNATURE_VERSION")
+AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+
+# AWS S3 Security Settings
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_QUERYSTRING_AUTH = False
+
+# Cache Settings
+AWS_S3_OBJECT_PARAMETERS = {
+    "CacheControl": "max-age=604800",
+}
+
+# Storage Configuration
 STORAGES = {
     "default": {
         "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
         "OPTIONS": {
             "location": "media",
+            "querystring_auth": False,
         },
     },
     "staticfiles": {
         "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
         "OPTIONS": {
             "location": "static",
+            "querystring_auth": False,
         },
     },
 }
 
-AWS_ACCESS_KEY_ID = env.str("AWS_S3_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = env.str("AWS_S3_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME = env.str("AWS_STORAGE_BUCKET_NAME")
-AWS_S3_REGION_NAME = env.str("AWS_S3_REGION_NAME")
-AWS_S3_SIGNATURE_VERSION = env.str("AWS_S3_SIGNATURE_VERSION")
+# URLs Configuration
+STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
+MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
