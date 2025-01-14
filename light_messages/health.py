eebfb5a5ab584c03872d6204f37eec42abd1d19a
abdelslam1997 <51970604+abdelslam1997@users.noreply.github.com>
2025-01-14 13:27:03 +0400

    Allow public access for health check : build-docker-image

--- light_messages/health.py
+++ light_messages/health.py
@@ -1,10 +1,12 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
 
 import os
 
 @api_view(['GET'])
+@permission_classes([AllowAny])
 def health_check(request):
     """
     A simple health check endpoint that returns 200 OK
